d76a4db483b373b579285eec7f8e97f9fa49899d
Anthony González <asgtoribio@gmail.com>
2025-06-26 01:47:31 +0000

    correccion

--- src/js/app.js
+++ src/js/app.js
@@ -41,7 +41,7 @@ function render(variables = {}) {
             <li><a href="https://twitter.com/${variables.twitter ||
               "4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github ||
-              "4geeksacademy"}><i class="fab fa-github"></i></a></li>
+              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
